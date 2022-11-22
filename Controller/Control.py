904576fec13c63be3e83820638e184497469be98
da2nes <daguas@espol.edu.ec>
2022-11-21 23:04:58 -0500

    Revisión

---
+++ Controller/Control.py
@@ -0,0 +1,11 @@
+#%% Import libraries and functions
+import xlwings as xw
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from PVT_Correlations.Model.Functions import Bo
+from PVT_Correlations.Model.Functions import Pb
+from PVT_Correlations.Model.Functions import Rs
+from PVT_Correlations.Model.Functions import uo
+#%%
