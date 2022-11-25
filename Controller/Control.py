29043bf464fd63209f45812dd9cf4fb15579ce94
ksls2 <kelosori@espol.edu.ec>
2022-11-24 19:23:21 -0500

    creacion de funcion main

--- Controller/Control.py
+++ Controller/Control.py
@@ -37,4 +37,40 @@ CORRELACION_AL = "AL_Marhoun"
 CORRELACION_B = "Beggs & Robinson"
 CORRELACION_G = "Glaso"
 
+#%%
+##################################
+def main():
+    wb=xw.Book.caller()
+    sheet=wb.sheets[SHEET_SUMMARY]
 
+    df_TVD = sheet[STOC_VALUES].options(pd.DataFrame, index=False, expand="table").value
+    input_col_names = df_TVD["Valores"].to_list()
+    Rs_value, Yo_value, Yg_value, T_value, P_value, API_value, BASIO, BASIO2 = tuple(
+        input_col_names
+    )
+    inpt_idx = [CORRELACION_S, CORRELACION_AL]
+    resultsBo = {}
+    resultsPb = {}
+    resultsRs = {}
+    for col in inpt_idx:
+        print(col)
+        resultsBo[col] = Bo(col, Rs_value, Yg_value, Yo_value, T_value)
+        resultsPb[col] = Pb(col, Rs_value, Yg_value, T_value, API_value, Yo_value)
+        resultsRs[col] = Rs(col, P_value, API_value, T_value, Yg_value, Yo_value)
+    inpt_idx2 = [CORRELACION_B, CORRELACION_G]
+    resultsuo = {}
+    for col2 in inpt_idx2:
+        resultsuo[col2] = uo(col2, API_value, T_value)
+
+    PVT_summary_result = [
+            resultsBo[CORRELACION_S],
+            resultsBo[CORRELACION_AL],
+            resultsPb[CORRELACION_S],
+            resultsPb[CORRELACION_AL],
+            resultsRs[CORRELACION_S],
+            resultsRs[CORRELACION_AL],
+            resultsuo[CORRELACION_B],
+            resultsuo[CORRELACION_G],
+    ]
+    sheet[BO_STANDING].options(transpose=True).value = PVT_summary_result
+    print(PVT_summary_result)
