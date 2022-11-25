855931c71aeeede29cb91576e8d169308dde69c9
ksls2 <kelosori@espol.edu.ec>
2022-11-24 19:33:16 -0500

    creacion de funcion main, correcion

--- Controller/Control.py
+++ Controller/Control.py
@@ -49,28 +49,34 @@ def main():
         input_col_names
     )
     inpt_idx = [CORRELACION_S, CORRELACION_AL]
-    resultsBo = {}
-    resultsPb = {}
-    resultsRs = {}
+    result_Bo = {}
+    result_Pb = {}
+    result_Rs = {}
     for col in inpt_idx:
         print(col)
-        resultsBo[col] = Bo(col, Rs_value, Yg_value, Yo_value, T_value)
-        resultsPb[col] = Pb(col, Rs_value, Yg_value, T_value, API_value, Yo_value)
-        resultsRs[col] = Rs(col, P_value, API_value, T_value, Yg_value, Yo_value)
+        result_Bo[col] = Bo(col, Rs_value, Yg_value, Yo_value, T_value)
+        result_Pb[col] = Pb(col, Rs_value, Yg_value, T_value, API_value, Yo_value)
+        result_Rs[col] = Rs(col, P_value, API_value, T_value, Yg_value, Yo_value)
     inpt_idx2 = [CORRELACION_B, CORRELACION_G]
-    resultsuo = {}
-    for col2 in inpt_idx2:
-        resultsuo[col2] = uo(col2, API_value, T_value)
 
-    PVT_summary_result = [
-            resultsBo[CORRELACION_S],
-            resultsBo[CORRELACION_AL],
-            resultsPb[CORRELACION_S],
-            resultsPb[CORRELACION_AL],
-            resultsRs[CORRELACION_S],
-            resultsRs[CORRELACION_AL],
-            resultsuo[CORRELACION_B],
-            resultsuo[CORRELACION_G],
+    result_uo = {}
+    for col2 in inpt_idx2:
+        result_uo[col2] = uo(col2, API_value, T_value)
+
+    PVT_summary_results = [
+            result_Bo[CORRELACION_S],
+            result_Bo[CORRELACION_AL],
+            result_Pb[CORRELACION_S],
+            result_Pb[CORRELACION_AL],
+            result_Rs[CORRELACION_S],
+            result_Rs[CORRELACION_AL],
+            result_uo[CORRELACION_B],
+            result_uo[CORRELACION_G],
     ]
-    sheet[BO_STANDING].options(transpose=True).value = PVT_summary_result
-    print(PVT_summary_result)
+    sheet[BO_STANDING].options(transpose=True).value = PVT_summary_results
+    print(PVT_summary_results)
+
+
+if __name__ == "__main__":
+    xw.Book("control.xlsm").set_mock_caller()
+    main()
